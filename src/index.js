1c0b08b66ed935d86a135b40756c639da98bbf41
Stephen Saunders <saunders.stephen@gmail.com>
2015-05-18 10:33:02 -0700

    changed the index/src

--- src/index.js
+++ src/index.js
@@ -1,219 +1,68 @@
 'use strict';
 
-const vkey = require('vkey');
+const keycode = require('keycode');
 
-const codes = {
-  '<shift>': '',
-  '<backspace>': 'BACKSPACE',
-  '<tab>': 'TAB',
-  '<control>': '',
-  '<escape>': 'ESC',
-  '<alt>': '',
-  '<home>': 'HOME',
-  '<end>': 'END',
-  '<meta>': 'META',
-  '<space>': 'SPACE'
-}
-
+const exclude = [
+  keycode('SHIFT'),
+  keycode('CTRL'),
+  keycode('CMD'),
+  keycode('ALT')
+];
 
 function Iggins(app, opts, done){
 
-  let combo = {
-    CTRL_A: {
-      description: 'Select all text in current source code',
-      action: ''
-    },
-    CTRL_C: {
-      description: 'Copy selected text to the clipboard',
-      action: ''
-    },
-    CTRL_F: {
-      description: 'Find or replace text',
-      action: ''
-    },
-    CTRL_V: {
-      description: 'Paste text from clipboard to selected area',
-      action: ''
-    },
-    CTRL_X: {
-      description: 'Cut selected text to the clipboard',
-      action: ''
-    },
-    CTRL_Z: {
-      description: 'Undo last action (unlimited)',
-      action: ''
-    },
-    CTRL_SHIFT_Z: {
-      description: 'Redo last action (unlimited)',
-      action: ''
-    },
-    TAB: {
-      description: 'Indent block (Inserts space characters)',
-      action: ''
-    },
-    SHIFT_TAB: {
-      description: 'Outdent block (Deletes space characters)',
-      action: ''
-    },
-    F3: {
-      description: 'Find text again',
-      action: ''
-    },
-    Shift_F3: {
-      description: 'Find text again (opposite direction)',
-      action: ''
-    },
-    F4: {
-      description: 'Replace current found selection',
-      action: ''
-    },
-    CTRL_F4: {
-      description: 'Perform replace and find next',
-      action: ''
-    },
-    CTRL_HOME: {
-      description: 'Jump to top of file',
-      action: ''
-    },
-    CTRL_END: {
-      description: 'Jump to end of file',
-      action: ''
-    },
-    // CTRL_UPARROW: {
-    //   description: 'Move source view up one line without moving cursor',
-    //   action: ''
-    // },
-    // CTRL_DOWNARROW: {
-    //   description: 'Move source view down one line without moving cursor',
-    //   action: ''
-    // },
-    CTRL_MINUS: {
-      description: 'decrease font size',
-      action: ''
-    },
-    CTRL_EQUALS: {
-      description: 'increase font size',
-      action: ''
-    },
-    // File Management
-
-    CTRL_O: {
-      description: 'Open source; may just focus file explorer',
-      action: ''
-    },
-    // CURSOR_ENTER: {
-    //   description: 'keys navigate and select files',
-    //   action: ''
-    // },
-    BACKSPACE: {
-      description: 'exits file view (project) (to project view)',
-      action: ''
-    },
-    CTRL_S: {
-      description: 'Save current source code file to its current location on disk',
-      action: ''
-    },
-    CTRL_P: {
-      description: 'Print current source',
-      action: ''
-    },
-    CTRL_TAB: {
-      description: 'Switch to next open source',
-      action: ''
-    },
-    CTRL_SHIFT_TAB: {
-      description: 'Switch to previous open source',
-      action: ''
-    },
-
-    // Code
-    CTRL_I: {
-      description: 'Identify BASIC Stamp firmware.',
-      action: ''
-    },
-    F6: {
-      description: 'Identify BASIC Stamp firmware.',
-      action: ''
-    },
-    CTRL_T: {
-      description: 'Perform a syntax check on the code.',
-      action: ''
-    },
-    F7: {
-      description: 'Perform a syntax check on the code.',
-      action: ''
-    },
-    CTRL_M: {
-      description: 'Tokenize and open Memory Map display.',
-      action: ''
-    },
-    F8: {
-      description: 'Tokenize and open Memory Map display.',
-      action: ''
-    },
-    CTRL_R: {
-      description: 'Tokenize, download to BASIC Stamp, and open Debug display if necessary.',
-      action: ''
-    },
-    F9: {
-      description: 'Tokenize, download to BASIC Stamp, and open Debug display if necessary.',
-      action: ''
-    },
-    ESC: {
-      description: 'Close current dialog.',
-      action: ''
-    }
-  };
+  let combo = {};
 
   // add keyCombos from the application
   function register(name, opt, fn){
     let keyCombo = name;
     let opts = opt;
     let handler = fn;
+    let description = '';
     if(typeof opts === 'function'){
       handler = opts;
       opts = {};
     }
 
-    let description = combo[keyCombo].description;
+    if (combo[keyCombo]){
+      description = combo[keyCombo].description;
+    }
 
     combo[keyCombo] = {
-      description: opts.description || description || '',
+      description: opts.description || description,
       action: handler
     }
   }
 
   // remove registered combos and events when components unmount
   function removeCombo(keyCombo){
-    delete combo[keyCombo];
-  }
+      delete combo[keyCombo];
+    }
 
   function match(e){
-    let kp = '';
-    let chName = e.key || e.keyCode || e.charCode; 
 
-    if(e.altKey){
-      kp.concat('ALT_');
-    } 
-    if(e.ctrlKey){
-      kp.concat('CTRL_');
-    }
-    if(e.shiftKey){
-      kp.concat('SHIFT_');
-    }
-
-    kp.concat(normalizeChar(chName));
-    if (combo[kp]){
-      combo[kp].action();
-    }
+    let keystroke = getKeystroke(e);
+    callAction(keystroke, combo);
   }
 
-  function normalizeChar(charCode){
-    if(typeof charCode === 'string'){
-      return charCode.toUpperCase();
+  function getKeystroke(kbEvent){
+    if (exclude.indexOf(kbEvent.keyCode) !== -1){
+      return;
     }
-    let output = vkey(charCode);
-    return codes[output] || output;
+
+    let alt = kbEvent.altKey ? 'ALT_' : ''
+    let meta = kbEvent.metaKey ? 'META_' : ''
+    let ctrl = kbEvent.ctrlKey ? 'CTRL_' : ''
+    let shift = kbEvent.shiftKey ? 'SHIFT_' : ''
+    let ch = kbEvent.key ? kbEvent.key.toUpperCase() : keycode(kbEvent.keyCode).toUpperCase()
+
+    return `${alt}${meta}${ctrl}${shift}${ch}`
+  }
+
+  function callAction(keystroke, directory){
+    if(directory[keystroke]){
+      directory[keystroke].action();
+    }
   }
 
   app.expose('keypress', {
@@ -223,7 +72,7 @@ function Iggins(app, opts, done){
   });
 
   if (window){
-    window.addEventListener('keyCapture', this.match);
+    window.addEventListener('keydown', match);
   }
 
   done();

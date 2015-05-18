33c241df2199ac4bc8423f7d6293d2541ace78ee
Stephen Saunders <saunders.stephen@gmail.com>
2015-05-18 13:55:58 -0700

    added lodash to package.json

--- src/index.js
+++ src/index.js
@@ -1,168 +1,8 @@
 'use strict';
 
-const vkey = require('vkey');
-
-const codes = {
-  '<shift>': '',
-  '<backspace>': 'BACKSPACE',
-  '<tab>': 'TAB',
-  '<control>': '',
-  '<escape>': 'ESC',
-  '<alt>': '',
-  '<home>': 'HOME',
-  '<end>': 'END',
-  '<meta>': 'META',
-  '<space>': 'SPACE'
-}
-
+const _ = require('lodash');
 
 function Iggins(app, opts, done){
-
-  let combo = {
-    CTRL_A: function({ ctrlKey, keyCode }){
-      return (ctrlKey === true && keyCode === 12);
-    },
-    CTRL_C: {
-      description: 'Copy selected text to the clipboard',
-      action: ''
-    },
-    CTRL_F: {
-      description: 'Find or replace text',
-      action: ''
-    },
-    CTRL_V: {
-      description: 'Paste text from clipboard to selected area',
-      action: ''
-    },
-    CTRL_X: {
-      description: 'Cut selected text to the clipboard',
-      action: ''
-    },
-    CTRL_Z: {
-      description: 'Undo last action (unlimited)',
-      action: ''
-    },
-    CTRL_SHIFT_Z: {
-      description: 'Redo last action (unlimited)',
-      action: ''
-    },
-    TAB: {
-      description: 'Indent block (Inserts space characters)',
-      action: ''
-    },
-    SHIFT_TAB: {
-      description: 'Outdent block (Deletes space characters)',
-      action: ''
-    },
-    F3: {
-      description: 'Find text again',
-      action: ''
-    },
-    Shift_F3: {
-      description: 'Find text again (opposite direction)',
-      action: ''
-    },
-    F4: {
-      description: 'Replace current found selection',
-      action: ''
-    },
-    CTRL_F4: {
-      description: 'Perform replace and find next',
-      action: ''
-    },
-    CTRL_HOME: {
-      description: 'Jump to top of file',
-      action: ''
-    },
-    CTRL_END: {
-      description: 'Jump to end of file',
-      action: ''
-    },
-    // CTRL_UPARROW: {
-    //   description: 'Move source view up one line without moving cursor',
-    //   action: ''
-    // },
-    // CTRL_DOWNARROW: {
-    //   description: 'Move source view down one line without moving cursor',
-    //   action: ''
-    // },
-    CTRL_MINUS: {
-      description: 'decrease font size',
-      action: ''
-    },
-    CTRL_EQUALS: {
-      description: 'increase font size',
-      action: ''
-    },
-    // File Management
-
-    CTRL_O: {
-      description: 'Open source; may just focus file explorer',
-      action: ''
-    },
-    // CURSOR_ENTER: {
-    //   description: 'keys navigate and select files',
-    //   action: ''
-    // },
-    BACKSPACE: {
-      description: 'exits file view (project) (to project view)',
-      action: ''
-    },
-    CTRL_S: {
-      description: 'Save current source code file to its current location on disk',
-      action: ''
-    },
-    CTRL_P: {
-      description: 'Print current source',
-      action: ''
-    },
-    CTRL_TAB: {
-      description: 'Switch to next open source',
-      action: ''
-    },
-    CTRL_SHIFT_TAB: {
-      description: 'Switch to previous open source',
-      action: ''
-    },
-
-    // Code
-    CTRL_I: {
-      description: 'Identify BASIC Stamp firmware.',
-      action: ''
-    },
-    F6: {
-      description: 'Identify BASIC Stamp firmware.',
-      action: ''
-    },
-    CTRL_T: {
-      description: 'Perform a syntax check on the code.',
-      action: ''
-    },
-    F7: {
-      description: 'Perform a syntax check on the code.',
-      action: ''
-    },
-    CTRL_M: {
-      description: 'Tokenize and open Memory Map display.',
-      action: ''
-    },
-    F8: {
-      description: 'Tokenize and open Memory Map display.',
-      action: ''
-    },
-    CTRL_R: {
-      description: 'Tokenize, download to BASIC Stamp, and open Debug display if necessary.',
-      action: ''
-    },
-    F9: {
-      description: 'Tokenize, download to BASIC Stamp, and open Debug display if necessary.',
-      action: ''
-    },
-    ESC: {
-      description: 'Close current dialog.',
-      action: ''
-    }
-  };
 
   const handlers = [];
 
@@ -172,53 +12,19 @@ function Iggins(app, opts, done){
       predicate: predicate,
       handler: handler
     });
-    // let keyCombo = name;
-    // let opts = opt;
-    // let handler = fn;
-    // if(typeof opts === 'function'){
-    //   handler = opts;
-    //   opts = {};
-    // }
-
-    // let description = combo[keyCombo].description;
-
-    // combo[keyCombo] = {
-    //   description: opts.description || description || '',
-    //   action: handler
-    // }
   }
 
   // remove registered combos and events when components unmount
-  function removeCombo(keyCombo){
-    delete combo[keyCombo];
+  function removeCombo(predicate){
+    handlers = _.reject(handlers, {'predicate', predicate});
   }
 
   function match(e){
-    let kp = '';
-    let chName = e.key || e.keyCode || e.charCode; 
-
-    if(e.altKey){
-      kp.concat('ALT_');
-    } 
-    if(e.ctrlKey){
-      kp.concat('CTRL_');
-    }
-    if(e.shiftKey){
-      kp.concat('SHIFT_');
-    }
-
-    kp.concat(normalizeChar(chName));
-    if (combo[kp]){
-      combo[kp].action();
-    }
-  }
-
-  function normalizeChar(charCode){
-    if(typeof charCode === 'string'){
-      return charCode.toUpperCase();
-    }
-    let output = vkey(charCode);
-    return codes[output] || output;
+    _.forEach(handlers, function(handlerObject){
+      if(handlerObject.predicate(evt)){
+        handlerObject.handler(evt);
+      }
+    });
   }
 
   app.expose('keypress', {
@@ -228,16 +34,12 @@ function Iggins(app, opts, done){
   });
 
   if (window){
-    window.addEventListener('keydown', function(evt){
-      _.forEach(handlers, function(handlerObject){
-        if(handlerObject.predicate(evt)){
-          handlerObject.handler(evt);
-        }
-      })
-    });
+    window.addEventListener('keydown', match(evt));
   }
 
   done();
 };
 
 module.exports = Iggins;
+
+app.keypress(app.keypress.CTRL_A, handler);
---
+++ src/predicates.js
@@ -0,0 +1,150 @@
+'use strict';
+
+const kc = require('keycode');
+
+module.exports = {
+  CTRL_A: function({ ctrlKey, keyCode }){
+    return ((ctrlKey === true || metaKey === true) && keyCode === kc('a'));
+  },
+  CTRL_C: function({ ctrlKey, keyCode }){
+    return ((ctrlKey === true || metaKey === true) && keyCode === kc('c'));
+  },
+  CTRL_F: function({ ctrlKey, keyCode }){
+    return ((ctrlKey === true || metaKey === true) && keyCode === kc('f'));
+  },
+  CTRL_V: function({ ctrlKey, keyCode }){
+    return ((ctrlKey === true || metaKey === true) && keyCode === kc('v'));
+  },
+  CTRL_X: function({ ctrlKey, keyCode }){
+    return ((ctrlKey === true || metaKey === true) && keyCode === kc('x'));
+  },
+  CTRL_Z: function({ ctrlKey, keyCode }){
+    return ((ctrlKey === true || metaKey === true) && keyCode === kc('z'));
+  },
+  CTRL_SHIFT_Z: function({ ctrlKey, keyCode }){
+    return ((ctrlKey === true || metaKey === true) && shiftKey === true && keyCode === kc('z'));
+  },
+  TAB: function({ ctrlKey, shiftKey, altKey, metaKey, keyCode }){
+    return (ctrlKey === false &&
+      shiftKey === false &&
+      altKey === false &&
+      metaKey === false &&
+      keyCode === kc('tab'));
+  },
+  SHIFT_TAB: function({ shiftKey, keyCode }){
+    return (shiftKey === true && keyCode === kc('tab'));
+  },
+  F3: function({ ctrlKey, shiftKey, altKey, metaKey, keyCode }){
+    return (ctrlKey === false &&
+      shiftKey === false &&
+      altKey === false &&
+      metaKey === false &&
+      keyCode === kc('f3'));
+  },
+  Shift_F3: function({ shiftKey, keyCode }){
+    return (shiftKey === true && keyCode === kc('f3'));
+  },
+  F4: function({ ctrlKey, shiftKey, altKey, metaKey, keyCode }){
+    return (ctrlKey === false &&
+      shiftKey === false &&
+      altKey === false &&
+      metaKey === false &&
+      keyCode === kc('f4'));
+  },
+  CTRL_F4: function({ ctrlKey, keyCode }){
+    return (ctrlKey === true && keyCode === kc('f4'));
+  },
+  CTRL_HOME: function({ ctrlKey, keyCode }){
+    return (ctrlKey === true && keyCode === kc('home'));
+  },
+  CTRL_END: function({ ctrlKey, keyCode }){
+    return (ctrlKey === true && keyCode === kc('end'));
+  },
+  CTRL_UP: function({ ctrlKey, keyCode }){
+    return ((ctrlKey === true || metaKey === true) && keyCode === kc('up'));
+  },
+  CTRL_DOWN: function({ ctrlKey, keyCode }){
+    return ((ctrlKey === true || metaKey === true) && keyCode === kc('down'));
+  },
+  CTRL_MINUS: function({ ctrlKey, keyCode }){
+    return ((ctrlKey === true || metaKey === true) && keyCode === kc('-'));
+  },
+  CTRL_EQUALS: function({ ctrlKey, keyCode }){
+    return ((ctrlKey === true || metaKey === true) && keyCode === kc('='));
+  },
+  // File Management
+
+  CTRL_O: function({ ctrlKey, keyCode }){
+    return ((ctrlKey === true || metaKey === true) && keyCode === kc('o'));
+  },
+  BACSPACE: function({ ctrlKey, shiftKey, altKey, metaKey, keyCode }){
+    return (ctrlKey === false &&
+      shiftKey === false &&
+      altKey === false &&
+      metaKey === false &&
+      keyCode === kc('backspace'));
+  },
+  CTRL_S: function({ ctrlKey, keyCode }){
+    return ((ctrlKey === true || metaKey === true) && keyCode === kc('s'));
+  },
+  CTRL_P: function({ ctrlKey, keyCode }){
+    return ((ctrlKey === true || metaKey === true) && keyCode === kc('p'));
+  },
+  CTRL_TAB: function({ ctrlKey, keyCode }){
+    return ((ctrlKey === true || metaKey === true) && keyCode === kc('tab'));
+  },
+  CTRL_SHIFT_TAB: function({ ctrlKey, shiftKey, keyCode }){
+    return ((ctrlKey === true || metaKey === true) &&
+      shiftKey === true &&
+      keyCode === kc('tab'));
+  },
+
+  // Code
+  CTRL_I: function({ ctrlKey, keyCode }){
+    return ((ctrlKey === true || metaKey === true) && keyCode === kc('i'));
+  },
+  F6: function({ ctrlKey, shiftKey, altKey, metaKey, keyCode }){
+    return (ctrlKey === false &&
+      shiftKey === false &&
+      altKey === false &&
+      metaKey === false &&
+      keyCode === kc('f6'));
+  },
+  CTRL_T: function({ ctrlKey, keyCode }){
+    return ((ctrlKey === true || metaKey === true) && keyCode === kc('t'));
+  },
+  F7: function({ ctrlKey, shiftKey, altKey, metaKey, keyCode }){
+    return (ctrlKey === false &&
+      shiftKey === false &&
+      altKey === false &&
+      metaKey === false &&
+      keyCode === kc('f7'));
+  },
+  CTRL_M: function({ ctrlKey, keyCode }){
+    return ((ctrlKey === true || metaKey === true) && keyCode === kc('m'));
+  },
+  F8: function({ ctrlKey, shiftKey, altKey, metaKey, keyCode }){
+    return (ctrlKey === false &&
+      shiftKey === false &&
+      altKey === false &&
+      metaKey === false &&
+      keyCode === kc('f8'));
+  },
+  CTRL_R: function({ ctrlKey, metaKey, keyCode }){
+    return ((ctrlKey === true || metaKey === true) && keyCode === kc('r'));
+  },
+  F9: function({ ctrlKey, shiftKey, altKey, metaKey, keyCode }){
+    return (ctrlKey === false &&
+      shiftKey === false &&
+      altKey === false &&
+      metaKey === false &&
+      keyCode === kc('f9'));
+  },
+  ESC: function({ ctrlKey, shiftKey, altKey, metaKey, keyCode }){
+    return (ctrlKey === false &&
+      shiftKey === false &&
+      altKey === false &&
+      metaKey === false &&
+      keyCode === kc('ESC'));
+  }
+};

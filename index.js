f7a9b1734586c3b6c01c7bf2af6f10977b85e3cf
Stephen Saunders <saunders.stephen@gmail.com>
2015-06-11 15:47:42 -0700

    remove examples and correct documentations from jsdocs

--- index.js
+++ index.js
@@ -4,40 +4,6 @@ var _ = require('lodash');
 
 var predicates = require('./predicates');
 
-/**
- * A plugin for Irken applications that exposes keyboard shortcut functionality.
- * @param  {object}   app  Instance of Irken application
- * @param  {object}  opts Options to be passed to Iggins plugin.
- * @param  {Function} done Noop function
- * @return {undefined}        No return value.
- * @example
- *
- * // Relies on having a Irken application.
- * var Irken = require('irken');
- * var app = new Irken();
- *
- * var plugins = [
- *   {
- *     register: require('iggins')
- *   }
- * ];
- *
- * // Is registered on an Irken applicaion as a plugin.
- * // This exposes the keypress functionality.
- * app.register(plugins, function(err){
- *  console.log('registered', err, app);
- *  app.render(function(err){
- *    console.log('rendered', err);
- *  });
- * });
- *
- * //For debugging
- * window.app = app;
- *
- * //Verify that the keypress function is exposed on the Irken application.
- * console.log(typeof app.keypress);
- * // => 'function'
- */
 function iggins(app, opts, done) {
 
   var handlers = [];
@@ -45,20 +11,10 @@ function iggins(app, opts, done) {
   /**
    * Registers a key combination and handler as an application keyboard shortcut.
    * @public
-   * @param  {string} predicate Has the form 'CTRL_L' or 'ALT_SHIFT_U'
-   * @param  {function} handler The action to invoke when keypress is detected.
+   * @memberOf iggins
+   * @param  {function} predicate Function returning true/false based on keydown events
+   * @param  {function} handler The action to invoke when predicate returns true.
    * @return {function}           A keypress remover function to be called by requestor on DOM unmount.
-   * @example
-   *
-   * var kp = iggins.keypress('ALT_K', function(){
-   *   console.log("Hello World!");
-   * });
-   *
-   * console.log(iggins.keypress.ALT_K);
-   * // => function ALT_K(evt)
-   *
-   * console.log(typeof kp);
-   * // => 'function'
    */
   function keypress(predicate, handler) {
     var keyAction = {

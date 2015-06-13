75dcef460425ac7b31e22a00db25fcc4a637353d
Stephen Saunders <saunders.stephen@gmail.com>
2015-06-12 18:27:09 -0700

    remove built index.js from git, add docs generation in gulpfile, update package.json

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var rename = require('gulp-rename');
+var gulpDocDown = require('gulp-docdown');
 
 function build(){
   return gulp.src('src/*.js')
@@ -9,4 +11,22 @@ function build(){
     .pipe(gulp.dest('./'));
 }
 
+function docs(){
+  var outputType = 'md';
+  var username = 'iceddev';
+  var project = 'iggins';
+  return gulp.src('./src/*.js')
+    .pipe(gulpDocDown({
+        title: 'API documentation / {fileName}',
+        url: 'https://github.com/' + username + '/' + project + '/blob/master/{filePath}',
+        toc: 'properties',
+        outputType : outputType,
+    }))
+    .pipe(rename({
+        extname: '.' + outputType
+    }))
+    .pipe(gulp.dest('./docs/'));
+}
+
+gulp.task('docs', docs);
 gulp.task('default', build);
